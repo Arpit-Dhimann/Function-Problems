571aad7af0976d6e8a8b6ec5bf100042a542bbdf
Arpit Dhiman <arpitdhimann@gmail.com>
2026-06-26 18:45:21 +0530

    add implement infinite loop using do-while

---
+++ Infinite_Loop.java
@@ -0,0 +1,16 @@
+//PROBLEM: Write an infinite loop using do while condition.
+
+import java.util.*;
+
+public class Infinite_Loop {
+   public static void main(String args[]) {
+
+      // Infinite loop using do-while
+      do {
+
+         System.out.println("This is an infinite loop!");
+
+      } while(true);
+      
+   }   
+}
